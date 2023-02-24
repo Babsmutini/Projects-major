dd705641440d099c1e16238aec61996b81bd00ec
babsmutini <scotteyawo@gmail.com>
2023-02-24 23:52:45 +0100

    Podcast widget implementation
    Init

---
+++ Podcaster/src/components/Podcasts/PodcastWidget.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PodcastWidget = ({img,title,summary,date, time}) => {
+  return (
+	<div>PodcastWidget</div>
+  )
+}
+
+export default PodcastWidget
---
+++ Podcaster/src/components/Podcasts/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Podcast = () => {
+  return (
+	<div>
+		<h1 className=' capitalize'>Podcasts you follow</h1>
+	</div>
+  )
+}
+
+export default Podcast
