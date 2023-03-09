e2b93ce2165f243c4c21b626de01e5c6cf93aee7
babsmutini <scotteyawo@gmail.com>
2023-03-09 23:42:35 +0100

    Added new podcast section

--- Podcaster/src/components/Podcasts/PodcastWidget.js
+++ Podcaster/src/components/Podcasts/PodcastWidget.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { BsFillPlayFill } from "react-icons/bs";
 
-const PodcastWidget = ({img,title,summary,date, time}) => {
-  return (
-	<div>PodcastWidget</div>
-  )
-}
+const PodcastWidget = ({ img, tag, title, summary, date, duration, duration_elapsed }) => {
+    return (
+            <div className="p-4 flex gap-4 min-w-[450px] bg-[#191934] overflow-auto rounded-lg ">
+                <img src={img} alt="" className="rounded-xl h-[200px] w-full " />
+                <div className="flex flex-col ">
+                    <p className="text-[#A965F7] uppercase text-[0.85em] ">{tag}</p>
+                    <h4 className="text-[1.2em] capitalize text-white font-semibold my-2 break-words">
+                        {title}
+                    </h4>
+                    <p className=" text-gray-400 ">{summary.slice("0", "46") + " ..."}</p>
+                    <div className="flex justify-between items-center mt-4">
+                        {duration_elapsed ? (
+                            <button className="bg-[#a965f7] flex items-center rounded-2xl py-1 px-2">
+                                <BsFillPlayFill className="text-[1.5em]" />
+                                Resume
+                            </button>
+                        ) : (
+                            date
+                        )}
 
-export default PodcastWidget
+                        <p className="text-white">
+                            {duration_elapsed ? `${duration_elapsed} left` : duration}
+                        </p>
+                    </div>
+                </div>
+            </div>
+      
+    );
+};
+
+export default PodcastWidget;
--- Podcaster/src/components/Podcasts/index.js
+++ Podcaster/src/components/Podcasts/index.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import "../Categories/styles.css"
+import PodcastWidget from './PodcastWidget';
+
+import img1 from "../../img/dean.jpeg"
+import img2 from "../../img/Earn_your_happy.jpg"
+import img3 from "../../img/download.jpeg"
 
 const Podcast = () => {
+	const data = [
+		{
+			image : img1,
+			tag: "CHURCHHOME",
+			title: "300.Who is sending you? - The Last Conversation",
+			summary: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, harum fugiat ducimus autem quod dolor. Laboriosam minima impedit omnis ipsam, nisi voluptates nostrum itaque! Dolorem ea beatae exercitationem eligendi? Corporis.",
+			duration: 21,
+			durationelapsed: 20,
+			date_posted: ""
+		},
+		{
+			image : img3,
+			tag: "CHURCHHOME",
+			title: "300.Who is sending you? - The Last Conversation",
+			summary: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, harum fugiat ducimus autem quod dolor. Laboriosam minima impedit omnis ipsam, nisi voluptates nostrum itaque! Dolorem ea beatae exercitationem eligendi? Corporis.",
+			duration: 21,
+			durationelapsed: 20,
+			date_posted: ""
+		},
+		{
+			image : img3,
+			tag: "CHURCHHOME",
+			title: "300.Who is sending you? - The Last Conversation",
+			summary: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, harum fugiat ducimus autem quod dolor. Laboriosam minima impedit omnis ipsam, nisi voluptates nostrum itaque! Dolorem ea beatae exercitationem eligendi? Corporis.",
+			duration: 21,
+			durationelapsed: 20,
+			date_posted: ""
+		},
+		{
+			image : img3,
+			tag: "CHURCHHOME",
+			title: "300.Who is sending you? - The Last Conversation",
+			summary: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, harum fugiat ducimus autem quod dolor. Laboriosam minima impedit omnis ipsam, nisi voluptates nostrum itaque! Dolorem ea beatae exercitationem eligendi? Corporis.",
+			duration: 21,
+			durationelapsed: 20,
+			date_posted: ""
+		},
+		
+
+	]
   return (
 	<div>
-		<h1 className=' capitalize'>Podcasts you follow</h1>
+		<div className='w-full flex justify-between mt-4 text-white'>
+		<h1 className='text-[1.5em] capitalize'>Podcasts you follow</h1>
+		<a href="/">View all</a>
+		</div>
+		<div className='flex gap-4 my-4 w-full overflow-x-scroll '>
+			{data.map((data,index) => (
+				<PodcastWidget key={index} img={data.image} tag={data.tag} title={data.title} summary={data.summary} duration={data.duration} duration_elapsed={data.durationelapsed} date={data.date_posted}/>
+			))}
+		</div>
+		
+
+
 	</div>
   )
 }
