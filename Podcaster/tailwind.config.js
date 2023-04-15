c2b6933fbae5f597700121a85803965a41dfc78c
babsmutini <scotteyawo@gmail.com>
2023-04-15 23:41:11 +0100

    Update configuration for tailwind to change viewport sizes

--- Podcaster/tailwind.config.js
+++ Podcaster/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens:{
+      'md': {'min': '826px'},
+      'lg': {'min': '1023px'},
+    },
+    extend: {
+      screens: {
+        'tablet': [{'min': '720px', 'max': '850px'},]
+        // => @media (min-height: 800px) { ... }
+      }
+    },
   },
   plugins: [
     require("@tailwindcss/line-clamp")
